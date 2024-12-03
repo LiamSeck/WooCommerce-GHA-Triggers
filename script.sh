d5574cbee2c859d735883281083846fcc1b1e203
Liam Seck <liamseck@live.ie>
2024-12-03 15:09:22 +0000

    updating script.sh

--- script.sh
+++ script.sh
@@ -7,6 +7,6 @@ ControlPersist 10m
 " ~/.ssh/config
 echo "$WPE_SSHG_KEY_PRIVATE" | tr -d '\r' > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
-# ssh -o UserKnownHostsFile=/dev/null -o LogLevel=quiet -o StrictHostKeyChecking=no -tt liamseprod@liamseprod.ssh.wpengine.net << EOF
-# cd ~/sites/${WPE_ENV};
-# wp action-scheduler run >> action-scheduler-run.log 2>> action-scheduler-run-error.log
+ssh -o UserKnownHostsFile=/dev/null -o LogLevel=quiet -o StrictHostKeyChecking=no -tt liamseprod@liamseprod.ssh.wpengine.net << EOF
+cd ~/sites/liamseprod/
+wp action-scheduler run >> action-scheduler-run.log 2>> action-scheduler-run-error.log
