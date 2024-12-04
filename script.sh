be067d888aa331a43859b2d1d103ffc79911b6be
Liam Seck <liamseck@live.ie>
2024-12-04 14:15:34 +0000

    updating workflow

--- script.sh
+++ script.sh
@@ -13,9 +13,12 @@ cd ~/sites/liamseprod/
 
 echo `date` >> action-scheduler-run.log 2>> action-scheduler-run-error.log
 echo "GitHub Action Run ID: $GHA_RUN_NO" >> action-scheduler-run.log 2>> action-scheduler-run-error.log
+echo "https://github.com/LiamSeck/WooCommerce-GHA-Triggers/actions/runs/$GHA_RUN_ID" >> action-scheduler-run.log 2>> action-scheduler-run-error.log
+
 
 echo `date` >> cron-run.log 2>> cron-run-error.log
 echo "GitHub Run ID: $GHA_RUN_NO" >> cron-run.log 2>> cron-run-error.log
+echo "https://github.com/LiamSeck/WooCommerce-GHA-Triggers/actions/runs/$GHA_RUN_ID" >> cron-run.log 2>> cron-run-error.log
 
 wp action-scheduler run >> action-scheduler-run.log 2>> action-scheduler-run-error.log
 wp cron event run --due-now >> cron-run.log 2>> cron-run-error.log
