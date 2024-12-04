6b298774aab595fd050cab4e9ab46e962fd01a38
Liam Seck <liamseck@live.ie>
2024-12-04 10:04:25 +0000

    updating script.sh

--- script.sh
+++ script.sh
@@ -12,9 +12,9 @@ ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa liamseprod@liamseprod.ssh.wpeng
 cd ~/sites/liamseprod/
 
 echo `date` >> action-scheduler-run.log 2>> action-scheduler-run-error.log
-echo "$GHA_RUN_ID" >> action-scheduler-run.log 2>> action-scheduler-run-error.log
+echo "$GHA_RUN_NO" >> action-scheduler-run.log 2>> action-scheduler-run-error.log
 echo `date` >> cron-run.log 2>> cron-run-error.log
-echo "$GHA_RUN_ID" >> cron-run.log 2>> cron-run-error.log
+echo "$GHA_RUN_NO" >> cron-run.log 2>> cron-run-error.log
 
 wp action-scheduler run >> action-scheduler-run.log 2>> action-scheduler-run-error.log
 wp cron event run --due-now >> cron-run.log 2>> cron-run-error.log
