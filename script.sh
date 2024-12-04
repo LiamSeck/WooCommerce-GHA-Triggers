27142ea64a17cbf84741d850a0678c4f9b6a992b
Liam Seck <liamseck@live.ie>
2024-12-04 09:54:47 +0000

    updating script.sh

--- script.sh
+++ script.sh
@@ -10,7 +10,11 @@ chmod 600 ~/.ssh/id_rsa
 # chmod 600 ~/.ssh/config
 ssh -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa liamseprod@liamseprod.ssh.wpengine.net << EOF
 cd ~/sites/liamseprod/
-echo `date` && "$GHA_RUN_ID" >> action-scheduler-run.log 2>> action-scheduler-run-error.log
-echo `date` && "$GHA_RUN_ID" >> cron-run.log 2>> cron-run-error.log
+
+echo `date` >> action-scheduler-run.log 2>> action-scheduler-run-error.log
+echo "$GHA_RUN_ID" >> action-scheduler-run.log 2>> action-scheduler-run-error.log
+echo `date` >> cron-run.log 2>> cron-run-error.log
+echo "$GHA_RUN_ID" >> cron-run.log 2>> cron-run-error.log
+
 wp action-scheduler run >> action-scheduler-run.log 2>> action-scheduler-run-error.log
 wp cron event run --due-now >> cron-run.log 2>> cron-run-error.log
